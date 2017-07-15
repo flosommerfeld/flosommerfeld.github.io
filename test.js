c211c56835cbdf89e1d1ae617218ab992ceabfb1
USE1ESS <flosommerfeld@users.noreply.github.com>
2017-07-15 12:23:51 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,16 +1,8 @@
 document.body.style.border = "5px solid red";
-
-if(document.title != "Historify"){
-console.log("fdafafa");
-  html2canvas(document.body, {
-    onrendered: function(canvas) {
-      var dataURL = canvas.toDataURL();
-      console.log(dataURL);
-      browser.storage.local.set({
-          element1: {
-            image: dataURL
-          },
-        });
-    }
-  });
-}
+html2canvas(document.body, {
+  onrendered: function(canvas) {
+    var dataURL = canvas.toDataURL();
+    console.log(dataURL);
+    window.open(dataURL);
+  }
+});
