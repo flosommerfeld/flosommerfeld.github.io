35c8e6c076ab4a8816841a13ce9de14bea3a1ec7
USE1ESS <flosommerfeld@users.noreply.github.com>
2017-07-17 13:38:27 +0200

    Delete test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-document.body.style.border = "5px solid red";
-html2canvas(document.body, {
-  onrendered: function(canvas) {
-    var dataURL = canvas.toDataURL();
-    console.log(dataURL);
-    window.open(dataURL);
-  }
-});
