4ef4d5ca2c2187dd5f2cb8d27e93d9343be22bbd
Florian Sommerfeld <help.flosommerfeld@gmail.com>
2018-05-13 21:37:27 +0200

    Fix issue #4 - Burger menu now automatically closes

--- js/mobile.js
+++ js/mobile.js
@@ -3,15 +3,29 @@ document.onreadystatechange = () => {//wait for the DOM to be ready
 
     var navBurger = document.getElementById("navBurger");
     var navMenu = document.getElementById("navbarMenuHeroA");
+    var classElements = document.getElementsByClassName("closeMenu");//all elements with the closeMenu class
+
+    function closeMenu(){
+      navBurger.className = "navbar-burger burger";
+      navMenu.className = "navbar-menu";
+    }
+
+    function openMenu(){
+      navBurger.className += " is-active";
+      navMenu.className += " is-active";
+    }
 
     navBurger.addEventListener("click",function(e){//event listener for the burger button
-      if(navBurger.className.indexOf("is-active") == -1){//open the menu
-        navBurger.className += " is-active";
-        navMenu.className += " is-active";
+      if(navBurger.className.indexOf("is-active") == -1){
+        openMenu();
+        for (let i = 0; i < classElements.length; i++) {//set an event listener for each element with the closeMenu class
+            classElements[i].addEventListener("click",function(e){
+              closeMenu();
+            }, false);
+	      }
       }
       else{//close the menu
-        navBurger.className = "navbar-burger burger";
-        navMenu.className = "navbar-menu";
+        closeMenu();
       }
     },false);
 
